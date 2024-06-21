7542df18f24c5d9ec3e4503d8f1344043d8ddca3
Chavdar Momchilov <chavmom@gmail.com>
2024-06-21 09:59:39 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   lecture01/Example02 	new file:   lecture01/Example02.cpp

---
+++ lecture01/Example02.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    std::cout << "Enter your name: ";
+    std::string s, s1;
+    std::cin >> s >> s1;
+    std::cout << s << " " << s1 << std::endl;
+    return 0; 
+}
